f8aceccedeefba170c46f8677682048c878b96b6
TethheTwo <TethheTwo@gmail.com>
2026-09-22 21:06:57 -0500

    fix(lsp): replace deprecated vim.lsp.codelens.refresh with enable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,10 +58,12 @@ return {
         end
 
         if client.server_capabilities.codeLensProvider then
-          vim.lsp.codelens.refresh()
+          vim.lsp.codelens.enable(true, { bufnr = bufnr })
           vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             buffer = bufnr,
-            callback = vim.lsp.codelens.refresh,
+            callback = function()
+              vim.lsp.codelens.enable(true, { bufnr = bufnr })
+            end,
           })
         end
       end
